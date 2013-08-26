85b5987e8634d020382bc51d3f1aede7b9868b39
zcl <28928400@qq.com>
2013-08-26 10:18:50 +0800

    完成

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ var http = require("http");
 var querystring = require('querystring');
 var url = require("url");
 var caipiao = require('./lib/caipiao');
-var test = require('./lib/test');
-
+var ems = require('./lib/ems');
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.writeHead(200, {'Content-Type': 'text/html,charset=utf8'});
+    req.setEncoding('utf8');
     var query = url.parse(req.url).query;
     var type = querystring.parse(query).type;
 
@@ -15,10 +15,16 @@ http.createServer(function (req, res) {
             res.end(result);
         })
 
+    } else if (type == "ems") {
+        var com = querystring.parse(query).com;
+        var nu = querystring.parse(query).nu;
+        ems.ems(com,nu,function (result) {
+              res.end(result);
+        })
+
     } else {
         res.end('end');
     }
-
 
 
 }).listen(4000);
