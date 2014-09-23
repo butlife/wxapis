2099ec513133bd91f26afe066775a2f05e4dfd58
zcl521ss <289528400@qq.com>
2014-09-23 18:11:01 +0800

    修复8684采集，输入错误公交导致程序错误的判断

--- bus/8684.js
+++ bus/8684.js
@@ -26,13 +26,21 @@ exports.line = function (line, callback) {
                     var buf = new Buffer(html, 'binary');
                     var str = iconv.decode(buf, 'gb2312');
                     var strs = str.split('"');
+
                     if (strs.length > 0) {
                         var info = strs[13] + '\r\n';
                         var money = strs[15] + '\r\n';
                         var lines = strs[27];
-                        lines = lines.substring(0, lines.indexOf('||')).trim();
-                        lines = lines.replace(/\|/g, ',') + '\r\n';
-                        var post = "安阳公交小喇叭提醒您：\r\n" + info + money + lines + "\r\n1、发送“G+站点名”查看对应公交站线路及车辆到站时间。\r\n2、发送“G”进行更多公交查询。";
+                        var post='';
+                        console.log(lines);
+                        if(lines){
+                            lines = lines.substring(0, lines.indexOf('||')).trim();
+                            lines = lines.replace(/\|/g, ',') + '\r\n';
+                            post = "安阳公交小喇叭提醒您：\r\n" + info + money + lines + "\r\n1、发送“G+站点名”查看对应公交站线路及车辆到站时间。\r\n2、发送“G”进行更多公交查询。";
+                        }else{
+                            post = "安阳公交小喇叭提醒您：\r\n没有此公交\r\n1、发送“G+站点名”查看对应公交站线路及车辆到站时间。\r\n2、发送“G”进行更多公交查询。";
+                        }
+
 
                         callback(post);
 
